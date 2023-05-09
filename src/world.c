74d22fda9e2532f1363506df6c641c0e69b82387
dusty-qw <71472647+dusty-qw@users.noreply.github.com>
2023-05-09 14:24:17 -0700

    Send spectator info to clients

--- src/world.c
+++ src/world.c
@@ -1776,6 +1776,7 @@ extern float intermission_exittime;
 void CheckTiming();
 void check_fcheck();
 void CheckTeamStatus();
+void SendSpecInfo();
 void DoMVDAutoTrack(void);
 void antilag_updateworld();
 
@@ -1854,6 +1855,8 @@ void StartFrame(int time)
 	check_monsters_respawn();
 
 	CheckTeamStatus();
+
+	SendSpecInfo();
 
 	CheckAutoXonX(true); // switch XonX mode dependant on players + specs count
 
