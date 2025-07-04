b7a52771d4bf718ce9ac8908f2f0edbe458ba059
Oscar Linderholm <osm@recv.se>
2025-07-04 18:37:54 +0200

    Add socd command
    The new command allows users to cycle between different SOCD detection
    modes:
    
    - Allow: Allow SOCD
    - Stats: Collect SOCD detection stats and print when the game ends
    - Warn: Display a warning when SOCD detection is triggered
    - Kick: Kick the player if SOCD detection is triggered

--- src/match.c
+++ src/match.c
@@ -1434,6 +1434,7 @@ void PrintCountdown(int seconds)
 	char *nowp = "";
 	char *matchtag = redtext(ezinfokey(world, "matchtag"));
 	qbool haveHead, haveBody, haveFoot;
+	int k_socd = cvar("k_socd");
 
 	strlcat(text, va("%s: %2s\n\n\n", redtext("Countdown"), dig3(seconds)), sizeof(text));
 
@@ -1698,6 +1699,12 @@ void PrintCountdown(int seconds)
 	{
 		strlcat(text, va("%s %4s\n", "Powerups", redtext(Get_PowerupsStr())), sizeof(text));
 	}
+
+	strlcat(text, va("%s %6s\n", SOCD_DETECTION_VERSION,
+		k_socd == SOCD_ALLOW ? redtext("allow")
+		: k_socd == SOCD_STATS ? redtext("stats")
+		: k_socd == SOCD_WARN ? redtext("warn")
+		: redtext("kick")), sizeof(text));
 
 	if (cvar("k_dmgfrags"))
 	{
